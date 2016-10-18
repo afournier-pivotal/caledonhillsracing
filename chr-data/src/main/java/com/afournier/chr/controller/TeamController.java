9320659362458d2d8266693baec5e9f64ee49517
afournier-pivotal <afournier@gopivotal.com>
2016-10-18 07:45:40 -0400

    Fixed cross origin.  Need to get angular working.

--- chr-data/src/main/java/com/afournier/chr/controller/TeamController.java
+++ chr-data/src/main/java/com/afournier/chr/controller/TeamController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.afournier.chr.model.Team;
 import com.afournier.chr.service.TeamService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/v1")
-
 public class TeamController {
 	
 	private TeamService teamService;
